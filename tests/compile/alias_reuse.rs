bc83c4f862df77b2106bd507ebc2a3386ffe3df0
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2025-06-04 01:30:53 +0200

    test: expand the alias-reuse test.
    Make sure that aliases re-used as arguments to functions are respected
    by the macro.

--- tests/compile/alias_reuse.rs
+++ tests/compile/alias_reuse.rs
@@ -1,10 +1,15 @@
 use compose_idents::compose_idents;
 
-compose_idents!(alias_1 = [foo, _, "baz"], alias_2 = [qux, _, alias_1], {
-    fn alias_2() -> u32 {
-        42
+compose_idents!(
+    alias_1 = [foo],
+    alias_2 = ["BAZ"],
+    alias_3 = [qux, _, alias_1, _, lower(alias_2)],
+    {
+        fn alias_3() -> u32 {
+            42
+        }
     }
-});
+);
 
 fn main() {
     assert_eq!(qux_foo_baz(), 42);
