6af3f27a916057c6d004c4434c599bc4be35b670
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2025-06-10 16:49:02 +0200

    test: expand compat test for bracket syntax with multi-arg func-call

--- tests/compile/brackets/bracket_syntax.rs
+++ tests/compile/brackets/bracket_syntax.rs
@@ -1,11 +1,18 @@
 use compose_idents::compose_idents;
 
-compose_idents!(my_fn = [foo, _, "baz"], {
-    fn my_fn() -> u32 {
-        42
-    }
-},);
+compose_idents!(
+    my_fn = [foo, _, "baz"],
+    my_static = [concat(gork, _, bork)],
+    {
+        static my_static: u32 = 42;
+
+        fn my_fn() -> u32 {
+            42
+        }
+    },
+);
 
 fn main() {
+    assert_eq!(gork_bork, 42);
     assert_eq!(foo_baz(), 42);
 }
