85f1bc2549630e10667bc40346531184a4990472
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2025-06-11 17:45:18 +0200

    dev: introduce combine!() helper for speculative parsing

--- src/parse.rs
+++ src/parse.rs
@@ -59,6 +59,59 @@ where
     }
 }
 
+/// Combines two syntactic elements into a single one and enables parsing them speculatively.
+///
+/// Useful for parsing multiple alternative kinds of terminators in one go.
+pub enum Combined<A, B>
+where
+    A: Parse,
+    B: Parse,
+{
+    A(A),
+    B(B),
+}
+
+impl<A, B> Parse for Combined<A, B>
+where
+    A: Parse,
+    B: Parse,
+{
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let span = input.span();
+        let mut errors = Vec::new();
+        let fork = input.fork();
+        match fork.parse::<A>() {
+            Ok(a) => return Ok(Self::A(a)),
+            Err(err) => errors.push(err),
+        }
+
+        let fork = input.fork();
+        match fork.parse::<B>() {
+            Ok(b) => return Ok(Self::B(b)),
+            Err(err) => errors.push(err),
+        }
+
+        Err(combine_errors(
+            "Unable to parse any of the combined tokens (see the errors below)",
+            span,
+            errors,
+        ))
+    }
+}
+
+/// Combines tokens into a single one that has a speculative [`Parse`] implemented for it.
+#[allow(unused_macros)]
+macro_rules! combine {
+    ($A:ty, $B:ty) => {
+        Combined::<$A, $B>
+    };
+    ($A:ty, $B:ty $(,$($T:ty),*)?) => {
+        combine!(Combined::<$A, $B>, $($T)*)
+    };
+}
+#[allow(unused_imports)]
+pub(crate) use combine;
+
 impl Parse for Arg {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let value: Arg;
