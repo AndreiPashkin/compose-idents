a8fd152e74ca82974fe970b97963c14522a2f57f
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2025-06-11 18:04:33 +0200

    fix: harden parsing of raw-token func args
    Makes raw-token argument being parsed explicitly.

--- tests/compile/funcs/normalize.rs
+++ tests/compile/funcs/normalize.rs
@@ -1,9 +1,15 @@
 use compose_idents::compose_idents;
 
-compose_idents!(my_var = concat(my_, normalize(&'static str)), {
-    const my_var: u32 = 42;
-});
+compose_idents!(
+    my_var_1 = concat(my_, normalize(&'static str)),
+    my_var_2 = concat(my_, normalize(Result<T, E>)),
+    {
+        const my_var_1: u32 = 42;
+        const my_var_2: u32 = 42;
+    }
+);
 
 fn main() {
     assert_eq!(my_static_str, 42);
+    assert_eq!(my_Result_T_E, 42);
 }
