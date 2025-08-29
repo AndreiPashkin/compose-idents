cab970627f21b7c0fe6f20570c82345be0fd8fbf
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2025-08-29 04:07:21 +0200

    dev: remove extraneous debug statements

--- src/parse/alias_spec_item.rs
+++ src/parse/alias_spec_item.rs
@@ -1,5 +1,4 @@
 use crate::ast::{Alias, AliasSpecItem, AliasValue};
-use crate::util::log::debug;
 use crate::util::unique_id::next_unique_id;
 use std::rc::Rc;
 use syn::parse::{Parse, ParseStream};
@@ -7,15 +6,10 @@ use syn::Token;
 
 impl Parse for AliasSpecItem {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        debug!("Parsing AliasSpecItem: {:?}...", input);
-
-        debug!("Parsing alias...");
         let alias: Alias = input.parse()?;
         input.parse::<Token![=]>()?;
 
-        debug!("Parsing alias value...");
         let value: AliasValue = input.parse()?;
-        debug!("Parsing AliasValue is successful.");
 
         Ok(AliasSpecItem::new(
             next_unique_id(),
